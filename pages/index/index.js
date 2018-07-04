195af674b126bea4ef6bf73cd8fd443098fe90e3
lbw1320028474 <1320028474@qq.com>
2018-07-04 22:55:06 +0800

    页面布局优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,51 +4,62 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    themesData: [
+      {
+        id: "1254",
+
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018667/preview_unlock_0.jpg"
+      },
+      {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018616/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018621/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018615/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018657/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018628/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018690/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018532/preview_unlock_0.jpg"
+      }, {
+        id: "1254",
+        name: "动态 夏威夷",
+        author: "小虫之家",
+        cover: "https://themecn.dbankcdn.com/dl/hitopdl/hitop/theme/700018337/preview_unlock_0.jpg"
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ]
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  onLoad: function () {
+
+  },
+  getUserInfo: function (e) {
+
   }
 })
